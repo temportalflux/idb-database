a900271201f5dadecfbc57fb8e29ef8d2c445806
temportalflux <dustin.yost.t@gmail.com>
2024-02-23 17:00:38 -0500

    Short-circuit cursors when the advance fails

--- src/cursor.rs
+++ src/cursor.rs
@@ -82,64 +82,73 @@ where
 	type Item = V;
 
 	fn poll_next(mut self: Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<Option<Self::Item>> {
-		loop {
-			// Process any pending advancement future first.
-			// If there is a future here, it means we are waiting for the underlying cursor
-			// to finish advancing before finding the next value.
-			if let Some(mut advance) = self.advance.take() {
-				match advance.as_mut().poll(cx) {
-					// the cursor is still advancing, poll the stream later
-					Poll::Pending => {
-						self.advance = Some(advance);
-						return Poll::Pending;
-					}
-					// advancing found an error, lets assume its the end of stream
-					Poll::Ready(Err(err)) => {
-						log::error!(target: "cursor", "Failed to advance idb::Cursor: {err:?}");
-						return Poll::Ready(None);
-					}
-					// the advancement has finished, we can resume the finding-of-next-value.
-					Poll::Ready(Ok(cursor)) => {
-						self.cursor = Some(cursor);
-					}
+		// Process any pending advancement future first.
+		// If there is a future here, it means we are waiting for the underlying cursor
+		// to finish advancing before finding the next value.
+		if let Some(mut advance) = self.advance.take() {
+			//log::debug!(target: "cursor", "polling advance");
+			match advance.as_mut().poll(cx) {
+				// the cursor is still advancing, poll the stream later
+				Poll::Pending => {
+					self.advance = Some(advance);
+					return Poll::Pending;
 				}
-			}
-
-			// There should ALWAYS be a cursor if we are not advancing and this stream was provided a cursor.
-			// If there is no cursor, then one was not provided by one of the `open_cursor` functions, so the stream is empty.
-			let Some(cursor) = self.cursor.take() else {
-				return Poll::Ready(None);
-			};
-
-			// Cursor exists and there is probably a next value, lets find out.
-			let value = match cursor.value() {
-				Ok(value) => value,
-				Err(err) => {
-					log::error!(target: "cursor", "Failed to get next value in idb::Cursor: {err:?}");
+				Poll::Ready(Err(idb::Error::CursorAdvanceFailed(_))) => {
 					return Poll::Ready(None);
 				}
-			};
-			// Value is empty, so we've reached end-of-stream.
-			if value.is_null() {
+				// advancing found an error, lets assume its the end of stream
+				Poll::Ready(Err(err)) => {
+					log::error!(target: "cursor", "Failed to advance idb::Cursor: {err:?}");
+					return Poll::Ready(None);
+				}
+				// the advancement has finished, we can resume the finding-of-next-value.
+				Poll::Ready(Ok(cursor)) => {
+					//log::debug!(target: "cursor", "cursor advanced completed");
+					self.cursor = Some(cursor);
+				}
+			}
+		}
+
+		// There should ALWAYS be a cursor if we are not advancing and this stream was provided a cursor.
+		// If there is no cursor, then one was not provided by one of the `open_cursor` functions, so the stream is empty.
+		let Some(cursor) = self.cursor.take() else {
+			return Poll::Ready(None);
+		};
+
+		// Cursor exists and there is probably a next value, lets find out.
+		//log::debug!(target: "cursor", "fetching current value");
+		let value = match cursor.value() {
+			Ok(value) => value,
+			Err(err) => {
+				log::error!(target: "cursor", "Failed to get next value in idb::Cursor: {err:?}");
 				return Poll::Ready(None);
 			}
-			// Parse the valid JSValue as the desired struct type.
-			let value = match serde_wasm_bindgen::from_value::<V>(value) {
-				Ok(value) => value,
-				Err(err) => {
-					log::error!(target: "cursor", "Failed to parse database value: {err:?}");
-					continue;
-				}
-			};
-			// Prime the advance future for the next loop or next time this stream is polled.
-			self.advance = Some(Box::pin(async move {
-				// move the cursor in so this future can have a static lifetime
-				let cursor = cursor;
-				cursor.advance(1)?.await?;
-				Ok(cursor)
-			}));
-			// Return the found value, while advancement run in the background.
-			return Poll::Ready(Some(value));
+		};
+		// Value is empty, so we've reached end-of-stream.
+		if value.is_null() {
+			return Poll::Ready(None);
 		}
+		//log::debug!(target: "cursor", "found value {:?}", value);
+		// Parse the valid JSValue as the desired struct type.
+		let value = match serde_wasm_bindgen::from_value::<V>(value) {
+			Ok(value) => value,
+			Err(err) => {
+				log::error!(target: "cursor", "Failed to parse database value: {err:?}");
+				return Poll::Ready(None);
+			}
+		};
+		
+		// Prime the advance future for the next loop or next time this stream is polled.
+		self.advance = Some(Box::pin(async move {
+			// move the cursor in so this future can have a static lifetime
+			let cursor = cursor;
+			//log::debug!(target: "cursor", "advancing cursor");
+			cursor.advance(1)?.await?;
+			//log::debug!(target: "cursor", "cursor advanced successfully");
+			Ok(cursor)
+		}));
+
+		// Return the found value, while advancement run in the background.
+		return Poll::Ready(Some(value));
 	}
 }
